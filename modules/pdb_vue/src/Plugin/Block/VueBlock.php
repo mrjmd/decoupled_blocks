2480c1a9e53cc069d3fc6be8be3df35b1c1de95b
Mark Miller <mrkmiller@ucdavis.edu>
2017-03-21 17:22:37 -0700

    Allow development mode to be set

--- modules/pdb_vue/src/Plugin/Block/VueBlock.php
+++ modules/pdb_vue/src/Plugin/Block/VueBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\pdb_vue\Plugin\Block;
 
 use Drupal\pdb\Plugin\Block\PdbBlock;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\pdb_vue\Render\VueMarkup;
 
 /**
@@ -14,7 +17,43 @@ use Drupal\pdb_vue\Render\VueMarkup;
  *   deriver = "\Drupal\pdb_vue\Plugin\Derivative\VueBlockDeriver"
  * )
  */
-class VueBlock extends PdbBlock {
+class VueBlock extends PdbBlock implements ContainerFactoryPluginInterface {
+
+  /**
+   * Stores the configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Creates a VueBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -44,6 +83,14 @@ class VueBlock extends PdbBlock {
     $attached = array();
     $attached['drupalSettings']['vue-apps'][$machine_name]['uri'] = '/' . $component['path'];
 
+    $config_settings = $this->configFactory->get('pdb_vue.settings');
+    if (isset($config_settings)) {
+      $attached['drupalSettings']['vue-apps']['development_mode'] = $config_settings->get('development_mode');
+    }
+    else {
+      $attached['drupalSettings']['vue-apps']['development_mode'] = FALSE;
+    }
+
     return $attached;
   }
 
@@ -53,12 +100,8 @@ class VueBlock extends PdbBlock {
   public function attachLibraries(array $component) {
     $parent_libraries = parent::attachLibraries($component);
 
-    $framework_libraries = array(
-      'pdb_vue/vue',
-    );
-
     $libraries = array(
-      'library' => array_merge($parent_libraries, $framework_libraries),
+      'library' => $parent_libraries,
     );
 
     return $libraries;
